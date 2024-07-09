31785ff4c449136646f6372e70bd87ce5f133387
Bartosz Hernas <b@hern.as>
2024-07-09 11:10:54 +0300

    New userUUID param

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const data = {
   clientId: "13036e5b-9c31-4a61-bbe6-ca2e01c0f412",
   keyId: "e5f7fa70cd",
   authModelUuid: "ddd950d8-3dc4-4018-b015-cd70ac66f9ed",
-  integrationUserUUID: "deb798d6-52d2-472d-a275-6c4d21477211",
+  integrationUserUUID: "7a17d9f2-6999-471e-a2a9-2d5960c7d74d",
   privateKey:
     "-----BEGIN EC PRIVATE KEY-----\n" +
     "MHcCAQEEIKfu++leP9pcWsZE53MUXLI0bY4gGsRIr7TbQigAA08GoAoGCCqGSM49\n" +
